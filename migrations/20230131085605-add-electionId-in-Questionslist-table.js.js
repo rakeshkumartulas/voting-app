da48a383c2b6d00a359bfebc6842d8756965d9f5
aprofessorrakeshkumar <aprof.rakesh@gmail.com>
2023-02-01 15:09:25 +0530

    change3

--- migrations/20230131085605-add-electionId-in-Questionslist-table.js.js
+++ migrations/20230131085605-add-electionId-in-Questionslist-table.js.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn("Questionslist", "electionId", {
+    await queryInterface.addColumn("Questionslists", "electionId", {
       type: Sequelize.DataTypes.INTEGER,
     });
     await queryInterface.addConstraint("Questionslists", {
